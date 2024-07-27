d92cad2eb460677cf7eda527cbdd0e4c54ec5355
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-07-27 19:57:23 +0300

    Add docstrings

--- find_bracelet_texts.py
+++ find_bracelet_texts.py
@@ -1,5 +1,19 @@
-
+"""
+Contains helper functions and main the recursive function for searching the best texts. 
+"""
 def can_form_string(input_string: str, letter_counts: dict[str, int]):
+    """
+    Checks if the input string can be formed with the given letter counts.
+
+    Args:
+        input_string (str): The string to check.
+        letter_counts (dict[str, int]): A dictionary of letter counts.
+            The keys are the letters and the values are the counts.
+
+    Returns:
+        bool: True if the string can be formed with the given letter counts,
+            False otherwise.
+    """
     # Calculate letter counts in input string.
     input_letter_counts: dict[str, int] = {}
     for letter in input_string:
@@ -14,6 +28,19 @@ def can_form_string(input_string: str, letter_counts: dict[str, int]):
     return True
 
 def update_remaining_letters(chosen_string: str, letter_counts: dict[str, int]):
+    """
+    Forms chosen string using the given letter pool and returns the updated letter pool.
+    Assumes that the chosen string can be formed with the given letter counts.
+    Does not modify the original letter counts dictionary.
+
+    Args:
+        chosen_string (str): The string to form.
+        letter_counts (dict[str, int]): A dictionary of letter counts.
+            The keys are the letters and the values are the counts.
+    
+    Returns:
+        dict[str, int]: The updated letter counts after forming the chosen string.
+    """
     updated_letter_counts: dict[str, int] = letter_counts.copy()
     for letter in chosen_string:
         # If the letter is in the dictionary, decrement its count
@@ -26,11 +53,27 @@ def update_remaining_letters(chosen_string: str, letter_counts: dict[str, int]):
     return updated_letter_counts
 
 def find_best_texts(letter_counts: dict[str, int], texts: list[str]) -> tuple[int, list[str]]:
-    # Recursive function to choose texts so that the total letter count is minimized.
-    # Returns a tuple of the lowest letter count possible and the list of texts chosen.
+    """
+    Finds the best texts to form with the given letter counts.
+    The best texts are the ones that minimize the total letter count.
+
+    Args:
+        letter_counts (dict[str, int]): A dictionary of letter counts.
+            The keys are the letters and the values are the counts.
+        texts (list[str]): A list of texts to choose from.
+
+    Returns:
+        tuple[int, list[str]]: A tuple of the lowest letter count possible and the 
+        list of texts chosen
+    """
+    # Inner recursive function
     def choose_text(current_text_idx: int, letters_left_total: int,
                     letters_left_dict: dict[str, int]) -> tuple[int, list[str]]:
-
+        """
+        Recursive function to choose the best texts to form with the given letter counts.
+        Tries all possible combinations of texts by including or excluding each text.
+        Is slow as heck for large inputs.
+        """
         if current_text_idx <  0:
             # out of texts to try
             return letters_left_total, []
