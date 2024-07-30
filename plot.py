52598d9269a49a5f156f6c246d4a418f3027a873
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-07-30 21:27:53 +0300

    Add plot of normalized letter difference

--- plot.py
+++ plot.py
@@ -8,8 +8,10 @@ from matplotlib import pyplot as plt
 from bracelet_texts import taylor_texts
 from find_bracelet_texts import can_form_string
 
+LETTER_COUNTS_FILENAME = "letter_counts_real.xlsx"
+
 # Read letter counts from xlsx file.
-letter_counts_df = pd.read_excel("letter_counts_real.xlsx", header=None)
+letter_counts_df = pd.read_excel(LETTER_COUNTS_FILENAME, header=None)
 letter_counts_dict = dict(zip(letter_counts_df[0], letter_counts_df[1]))
 
 # Make texts upper case and remove spaces.
@@ -53,6 +55,34 @@ for text in possible_texts:
 # Plot to letter counts in possible texts.
 plt.figure(2)
 plt.bar(letter_counts_in_texts.keys(), letter_counts_in_texts.values())
-plt.xlabel('Letter')
-plt.ylabel('Count')
+plt.xlabel("Letter")
+plt.ylabel("Count")
+plt.title("Letters in possible texts")
+
+# Calculate the normalized difference between the available letters
+# and the letters in the possible texts.
+
+total_available_letters = sum(letter_counts_dict.values())
+total_letters_in_texts = sum(letter_counts_in_texts.values())
+norm_letter_counts_dict = {
+    letter: count / total_available_letters
+    for letter, count in letter_counts_dict.items()
+}
+norm_letter_counts_in_possible_texts = {
+    letter: count / total_letters_in_texts
+    for letter, count in letter_counts_in_texts.items()
+}
+
+letter_difference = {}
+for letter in string.ascii_uppercase:
+    letter_difference[letter] = norm_letter_counts_dict.get(
+        letter, 0
+    ) - norm_letter_counts_in_possible_texts.get(letter, 0)
+
+# Plot the difference between the available letters and the letters in the possible texts.
+plt.figure(3)
+plt.bar(letter_difference.keys(), letter_difference.values())
+plt.xlabel("Letter")
+plt.ylabel("available - in texts")
+plt.title("Normalized diff between available letters and letters in possible texts")
 plt.show()
