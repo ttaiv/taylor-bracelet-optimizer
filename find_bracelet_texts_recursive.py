3e0c776bd69ca9492cb6517a564cbb29740a5e70
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-08-02 20:52:26 +0300

    Use tuple instead of dictionary to enable hashing

--- find_bracelet_texts_recursive.py
+++ find_bracelet_texts_recursive.py
@@ -5,14 +5,37 @@ Contains helper functions and main the recursive function for searching the best
 from collections import Counter
 
 
-def can_form_string(input_string: str, letter_counts: dict[str, int]):
+def letter_dict_to_tuple(letter_counts: dict[str, int]) -> tuple[int, ...]:
+    """
+    Converts a dictionary of letter counts to an array of letters.
+
+    Args:
+        letter_counts (dict[str, int]): A dictionary of letter counts.
+            The keys are the letters and the values are the counts.
+
+    Returns:
+        tuple[int, ...]: An array of letter counts. The index of the array
+            corresponds to the letter's position in the alphabet, starting from 'A' at index 0.
+    """
+    letters: list[int] = [0] * 26
+    for letter, count in letter_counts.items():
+        if not "A" <= letter <= "Z":
+            print(f"Invalid letter '{letter}' found in letter_counts, skipping.")
+            continue
+        idx: int = ord(letter) - ord("A")
+        letters[idx] = count
+
+    return tuple(letters)
+
+
+def can_form_string(input_string: str, letter_counts: tuple[int, ...]) -> bool:
     """
     Checks if the input string can be formed with the given letter counts.
 
     Args:
         input_string (str): The string to check.
-        letter_counts (dict[str, int]): A dictionary of letter counts.
-            The keys are the letters and the values are the counts.
+        letter_counts (tuple[int, ...]): A tuple of letter counts. The index of the tuple
+            corresponds to the letter's position in the alphabet, starting from 'A' at index 0.
 
     Returns:
         bool: True if the string can be formed with the given letter counts,
@@ -22,35 +45,35 @@ def can_form_string(input_string: str, letter_counts: dict[str, int]):
     input_letter_counts: dict[str, int] = Counter(input_string)
     # Check if we have enough letters.
     for letter, count in input_letter_counts.items():
-        if letter not in letter_counts or count > letter_counts[letter]:
+        if not "A" <= letter <= "Z" or count > letter_counts[ord(letter) - ord("A")]:
             return False
+
     return True
 
 
-def update_remaining_letters(chosen_string: str, letter_counts: dict[str, int]):
+def update_remaining_letters(
+    chosen_string: str, letter_counts: tuple[int, ...]
+) -> tuple[int, ...]:
     """
     Forms chosen string using the given letter pool and returns the updated letter pool.
     Assumes that the chosen string can be formed with the given letter counts.
-    Does not modify the original letter counts dictionary.
 
     Args:
         chosen_string (str): The string to form.
-        letter_counts (dict[str, int]): A dictionary of letter counts.
-            The keys are the letters and the values are the counts.
+        letter_counts (tuple[int, ...]): A tuple of letter counts. The index of the tuple
+            corresponds to the letter's position in the alphabet, starting from 'A' at index 0.
 
     Returns:
-        dict[str, int]: The updated letter counts after forming the chosen string.
+        tuple[int, ...]: The updated letter counts after forming the chosen string.
     """
-    updated_letter_counts: dict[str, int] = letter_counts.copy()
-    for letter in chosen_string:
-        # If the letter is in the dictionary, decrement its count
-        if letter in updated_letter_counts:
-            updated_letter_counts[letter] -= 1
-        # If the letter is not in the dictionary, this is an error state
-        else:
-            raise ValueError(f"Letter '{letter}' not found in letter_counts")
+    # Calculate letter counts in input string.
+    input_letter_counts: dict[str, int] = Counter(chosen_string)
+    # Update letter counts.
+    new_letter_counts: list[int] = list(letter_counts)
+    for letter, count in input_letter_counts.items():
+        new_letter_counts[ord(letter) - ord("A")] -= count
 
-    return updated_letter_counts
+    return tuple(new_letter_counts)
 
 
 def find_best_texts_recursive(
@@ -70,11 +93,13 @@ def find_best_texts_recursive(
         list of texts chosen and the number of made recursive calls.
     """
 
+    letter_counts_tuple = letter_dict_to_tuple(letter_counts)
+
     # Inner recursive function
     def choose_text(
         current_text_idx: int,
         letters_left_total: int,
-        letters_left_dict: dict[str, int],
+        letters_left: tuple[int, ...],
         recursive_calls: int,
     ) -> tuple[int, list[str], int]:
         """
@@ -89,19 +114,19 @@ def find_best_texts_recursive(
         # Try this text.
         current_text: str = texts[current_text_idx]
 
-        if not can_form_string(current_text, letters_left_dict):
+        if not can_form_string(current_text, letters_left):
             # exclude this text
             return choose_text(
                 current_text_idx - 1,
                 letters_left_total,
-                letters_left_dict,
+                letters_left,
                 recursive_calls + 1,
             )
 
         # Can form current text.
         # Search for the best texts to form with the remaining letters.
-        new_letters_left: dict[str, int] = update_remaining_letters(
-            current_text, letters_left_dict
+        new_letters_left: tuple[int, ...] = update_remaining_letters(
+            current_text, letters_left
         )
         inc_letters_left, incl_sub_sol, inc_sub_calls = choose_text(
             current_text_idx - 1,
@@ -112,7 +137,7 @@ def find_best_texts_recursive(
 
         # Compare the cases where this text is included or excluded.
         excl_letters_left, excl_sol, excl_sub_calls = choose_text(
-            current_text_idx - 1, letters_left_total, letters_left_dict, 1
+            current_text_idx - 1, letters_left_total, letters_left, 1
         )
         new_calls = recursive_calls + inc_sub_calls + excl_sub_calls
 
@@ -123,4 +148,6 @@ def find_best_texts_recursive(
 
     # Inner function ends
 
-    return choose_text(len(texts) - 1, sum(letter_counts.values()), letter_counts, 0)
+    return choose_text(
+        len(texts) - 1, sum(letter_counts.values()), letter_counts_tuple, 0
+    )
