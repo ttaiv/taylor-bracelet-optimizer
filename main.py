2b54f04c35b3bd36a7a083b219348008bb5cabf6
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-08-04 16:52:39 +0300

    Update main to filter texts and use ilp solution method

--- main.py
+++ main.py
@@ -3,12 +3,21 @@
 import pandas as pd
 from bracelet_texts import taylor_texts
 from find_bracelet_texts import find_best_texts
+from find_bracelet_texts_ilp import find_best_texts_ilp
 
 LETTER_COUNTS_DIR: str = "data"
-LETTER_COUNTS_FILENAME: str = "letter_counts_test_medium.xlsx"
+LETTER_COUNTS_FILENAME: str = "letter_counts_real.xlsx"
+SOLUTION_METHOD: str = "ILP"  # "ILP" or "recursive"
 
 # Make texts upper case and remove spaces
 texts: list[str] = [text.upper().replace(" ", "") for text in taylor_texts]
+
+# Remove texts that contain characters other than A-Z
+texts = [
+    text
+    for text in texts
+    if all(letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for letter in text)
+]
 
 # Read letter counts from xlsx file.
 path = f"{LETTER_COUNTS_DIR}/{LETTER_COUNTS_FILENAME}"
@@ -24,14 +33,19 @@ print()
 print(f"Starting the algorithm with a pool of {total_letter_count} letters.")
 print()
 
-lowest_letter_count, solution_texts, recursive_calls_count = find_best_texts(
-    letter_counts_dict, texts
+lowest_possible_letter_count, solution_texts, recursive_calls = (
+    (*find_best_texts_ilp(letter_counts_dict, texts), 0)  # produces prints
+    if SOLUTION_METHOD == "ILP"
+    else find_best_texts(letter_counts_dict, texts)
 )
 
 print(
-    f"You can decrease the letter count to {lowest_letter_count} by choosing the following texts:"
+    f"You can decrease the letter count to {lowest_possible_letter_count}"
+    " by choosing the following texts:"
 )
 for chosen_text in solution_texts:
     print(chosen_text)
 print()
-print(f"The algorithm made {recursive_calls_count} recursive calls.")
+
+if SOLUTION_METHOD == "recursive":
+    print(f"The algorithm made {recursive_calls} recursive calls.")
